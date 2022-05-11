57ca8aa2fa2013db920d10c73bc8e1efa607efe1
Yago Souza <yaago.souza@hotmail.com>
2022-05-10 23:57:54 -0300

    #TASK-9 fix: Removendo chave da API

--- app/src/main/java/com/yagosouza/yeahmovies/AppConstants.kt
+++ app/src/main/java/com/yagosouza/yeahmovies/AppConstants.kt
@@ -1,7 +1,7 @@
 package com.yagosouza.yeahmovies
 
 object AppConstants {
-    const val TMDB_API_KEY = "1d9de2ed12a0bc4140a40118cb0053b5"
+    const val TMDB_API_KEY = ""
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val TMDB_IMAGE_BASE_URL_W185 = "https://image.tmdb.org/t/p/w185/"
     const val TMDB_IMAGE_BASE_URL_W500 = "https://image.tmdb.org/t/p/w500/"
